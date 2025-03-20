1dc600581cad16f4766dbe62dacc80471b6374d6
kaleab <alebachewkaleab@gmail.com>
2025-03-20 09:50:34 +0300

    Frontend Setup

---
+++ frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {render} from "react-dom"
+
+export default function App(){
+    return(
+        <h1>Test 1</h1>
+    )
+}
+const appDiv = document.getElementById("app")
+render(<App />,appDiv)
